ee7eade704e8f6fe31addf4b9f81a258be3450da
Dhanush N <dhanushnacharya17@gmail.com>
2026-01-13 00:35:56 +0530

    segment registers are set

--- OS/boot.asm
+++ OS/boot.asm
@@ -1,7 +1,18 @@
-ORG 0x7c00
+ORG 0
 BITS 16
 
-start:  
+jmp 0x7c0:start
+
+start: 
+    cli ; clears the interrupts
+    mov ax,0x7c0
+    mov ds,ax
+    mov es,ax
+    mov ax,0x00
+    mov ss,ax
+    mov sp,0x7c00
+    sti ; enables the interrupts
+
     mov si,message
     call print
     jmp $
